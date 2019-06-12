84c918312cd4c257c5b51adf476a1ef36b466048
Melanie Faibish <mlf398@nyu.edu>
2019-06-12 00:08:56 -0400

    Separate capture and display processes

--- shopping_cart.py
+++ shopping_cart.py
@@ -33,20 +33,36 @@ products = [
 # Capturing User Inputs
 
 total_price = 0
-
+selected_items = []
 while True:
     selected_item = input("Please input a product identifier, or DONE if there are no more items: ")
     if selected_item == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_item)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-        
-    #print("SHOPPING CART ITEM IDENTIFIERS INCLUDE: " + str(shopping_list).translate(None))
+        selected_items.append(selected_item)
 
 
 # Info Display/Output
 
+#print(selected_items)
+
+for selected_item in selected_items:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_item)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+
+
+
 print("TOTAL PRICE: " + str(total_price))
+
+
+# A grocery store name of your choice
+# A grocery store phone number and/or website URL and/or address of choice
+# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
+# The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
+# The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+# The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# A friendly message thanking the customer and/or encouraging the customer to shop again
